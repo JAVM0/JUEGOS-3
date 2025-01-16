81252fb56a940c7982f96d1945ad5cc568387afe
JAV <javicho201410@gmail.com>
2025-01-15 21:32:23 -0500

    ya

--- SuperMangoPlataformer/main.js
+++ SuperMangoPlataformer/main.js
@@ -4,9 +4,9 @@ import { Level } from './utils/Level.js'
 import { load } from './utils/loader.js'
 import { level1Layout, level1Mappings } from './content/level1/level1Layout.js'
 import { Player } from './entities/Player.js'
-import {attachCamera} from './utils/camera.js'
+import { attachCamera } from './utils/camera.js'
 
-kaboom ({
+kaboom({
     width: 1280,
     height: 720,
     letterbox: true,
@@ -19,15 +19,15 @@ load.sounds()
 load.assets()
 
 const scenes = {
-    menu:() =>{
+    menu: () => {
         uiManager.displayMenu()
-     },
- 
-     controls:()=>{
+    },
+
+    controls: () => {
         uiManager.displayControlsMenu()
-     },
- 
-     1:()=>{
+    },
+
+    1: () => {
 
         setGravity(1400)
 
@@ -35,32 +35,32 @@ const scenes = {
         level1.drawBackground("forest-background")
         level1.drawMapLayout(level1Layout, level1Mappings)
 
-        const player = new Player(1500, 100, 400, 650, 3,1 , false)
+        const player = new Player(1500, 100, 400, 650, 3, 1, false)
 
-        attachCamera(player.gameObj, 0 , 200)
+        attachCamera(player.gameObj, 0, 200)
 
-        level1.drawWaves("water","wave")
-     },
- 
-     2:()=>{
-         
-     },
- 
-     3:()=>{
- 
-     },
- 
-     gameover: ()=>{
- 
-     },
- 
-     end:()=>{
- 
-     }
+        level1.drawWaves("water", "wave")
+    },
+
+    2: () => {
+
+    },
+
+    3: () => {
+
+    },
+
+    gameover: () => {
+
+    },
+
+    end: () => {
+
+    }
 }
 
-for (const key in scenes){
-    scene (key, scenes[key])
+for (const key in scenes) {
+    scene(key, scenes[key])
 }
 
 go("menu")
